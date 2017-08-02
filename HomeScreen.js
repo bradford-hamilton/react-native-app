f13f2aa5d2eeeb6b8cd1d90dc7b178fa4ccdb932
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-08-01 22:12:55 -0600

    HomeScreen and QuailZone components

---
+++ HomeScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static navigationOptions = { title: 'Welcome', };
+
+  render() {
+    let picture = { uri: 'http://bradford.cafnr.org/assets/sites/3/Bobwhite_Quail_0500.jpg', };
+    const { navigate } = this.props.navigation;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>My name is Bradford,</Text>
+        <Text style={styles.text}>you are using my app.</Text>
+        <Text style={styles.text}>Welcome to the Quail Zone.</Text>
+        <Image source={picture} style={styles.image} />
+        <Button
+          onPress={() => navigate('QuailZone')}
+          title="Enter only if you are 18+"
+          color="#5589ba"
+          accessibilityLabel="Learn more about this button"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+});
---
+++ QuailZone.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+export default class QuailZone extends React.Component  {
+  render() {
+    return <h1>Quail Zone</h1>;
+  }
+}
