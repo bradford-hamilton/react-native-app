2e1c2cdf7366183bf15837b78caeaca69d119446
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-08-02 20:32:02 -0600

    add navigation functionality

--- QuailZone.js
+++ QuailZone.js
@@ -3,7 +3,25 @@ import { StackNavigator } from 'react-navigation';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default class QuailZone extends React.Component  {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return <h1>Quail Zone</h1>;
+    return (
+        <View style={styles.container}>
+          <Text style={styles.text}>Quail Zone.</Text>
+        </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
